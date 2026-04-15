3aa33713c3e4aee337a4ca8c7d129124294b3638
Jay Zhou <spammyjunbai@gmail.com>
2026-04-15 13:45:16 +0800

    1

--- 30-days-terraform/2-static-website/main.tf
+++ 30-days-terraform/2-static-website/main.tf
@@ -5,10 +5,27 @@ provider "aws" {
 
 # S3 Resource
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "zhoujunbai_tf_jj1"
+  bucket = "${var.bucket_name}_jj1"
 
   tags = {
     Name = "My Bucket" 
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name = "demo-oac"
+  description = "Example Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior = "always"
+  signing_protocol = "sigv4"
 }
