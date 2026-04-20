b233b7e5462263093466a7ad58ec4b69ee5669b9
Jay Zhou <spammyjunbai@gmail.com>
2026-04-20 12:03:20 +0800

    1

--- 30-days-terraform/5-green-blue-deployment/variables.tf
+++ 30-days-terraform/5-green-blue-deployment/variables.tf
@@ -6,10 +6,32 @@ variable "profile" {
   default = "default"
 }
 
+variable "solution_stack_name" {
+  description = "Beanstalk solution stack name"
+  type = string
+  default = "64bit Amazon Linux 2023 v6.6.8 running Node.js 20"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance type for Beanstalk Env"
+  type = string
+  default = "t3.micro"
+}
+
 variable "bucket-name" {
   default = "zhoujunbai-tf-bucket"
 }
 
-variable "app-name" {
-  default = "main-app"
+variable "tags" {
+  description = "Tags to apply to all resource"
+  type = map(string)
+  default = {
+    "Project" = "BlueGreenDeployment"
+    "Env" = "Demo"
+    "ManagedBy" = "Terraform"
+  }
+}
+
+variable "app_name" {
+  default = "zhoujunbai-tf"
 }
